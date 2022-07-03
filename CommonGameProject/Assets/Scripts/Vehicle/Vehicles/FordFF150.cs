3fac09facaf0a64fc5ba10901704a76901ea7bd2
Olexiy Kachur <61654653+utochka228@users.noreply.github.com>
2022-07-04 01:03:22 +0300

    Vehicle import
    Imported 3 vehicles - Kraz, Mini bulldozer, Ford.
    Material and vehicle behaviour setup.

---
+++ CommonGameProject/Assets/Scripts/Vehicle/Vehicles/FordFF150.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vehicle.VehicleList
+{
+    public class FordFF150 : Car
+    {
+        protected override void OnInteractedWithSmth(List<Collider> interactedColliders)
+        {
+            
+        }
+    }
+}
