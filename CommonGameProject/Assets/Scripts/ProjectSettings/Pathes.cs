60cfff6713b43487e258d1e08fec854632ff0323
Olexiy Kachur <61654653+utochka228@users.noreply.github.com>
2022-07-24 02:45:16 +0300

    Bootstrapper, Project hierarchy, Pathes
    Project initialization - Bootstrapper.cs
    
    Project hierarchy - scripts, scenes.
    
    Pathes class for getting readonly pathes of this game.

---
+++ CommonGameProject/Assets/Scripts/ProjectSettings/Pathes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.ProjectSettings
+{
+    public static class Pathes
+    {
+        #region Resources
+
+        public static readonly string SystemsResourcesPath = "Systems";
+
+        #endregion
+    }
+}
